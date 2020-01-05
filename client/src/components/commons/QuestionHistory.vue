2a2e10398a1fe6ea2462fc71a52aafebb44780d8
papabyte <bcreze@protonmail.com>
2020-01-05 10:25:58 +0100

    history

--- client/src/components/commons/QuestionHistory.vue
+++ client/src/components/commons/QuestionHistory.vue
@@ -13,48 +13,43 @@
 				</a>
 		</div>
 		<div class="card-content scrollable">
-			<div v-for="(item,index) in historyItems" class="columns history-tile" :key="item.operation_id">
-				<div class="column is-1">	<h4 class="title is-4">{{historyItems.length - index}}</h4></div>
-
-				<div class="column">
-					<div v-if="item.event_type=='new_question'">
-						<div><b>Created</b> - {{item.time}}</div>
-						<div> Reward  <byte-amount :amount="question.reward"/></div>
-							By <user :address="item.author_address" :nickname="item.author_nickname"/>
-					</div>
-					<div v-if="item.event_type =='stake' || item.event_type=='initial_stake'" >
-						<span><b>{{item.event_type =='stake' ? 'Counter stake' : 'Initial stake'}} </b> - {{item.time}} </span>
+			<div class="container">
+					<div v-for="(item,index) in historyItems" class="row mt-1" :key="index">
 						<div>
-							<b><user :address="item.author_address" :nickname="item.author_nickname"/></b> staked <b>
-								<byte-amount :amount="item.accepted_amount"/></b> on <b>{{item.stake_on}}</b>
-						</div>
-						<div class="columns is-multiline">
-							<div class="column is-full">Resulting outcome: <b>{{item.new_outcome}}</b></div>
-							<div v-if="item.expected_reward" class="column is-full">Expected reward: <b><byte-amount :amount="item.expected_reward"/></b></div>
-							<div class="column is-half">Total staked on <b>yes</b>: <b><byte-amount :amount="item.total_staked_on_yes"/></b></div>
-							<div class="column is-half">Total staked on <b>no</b>: <b><byte-amount :amount="item.total_staked_on_no"/></b></div>
-						</div>
-					</div>
-					<div v-if="item.event_type =='commit'">
-						<div cols="12">
-							<span><b>Committed</b> - {{item.time}} </span>
-							<div class="pt-2">
-								<span v-if="item.paid_out_amount" class="d-block"><b><byte-amount :amount="item.paid_out_amount"/></b> paid to <b>{{item.paid_out_address}}</b></span>
+							<div class="box" v-if="item.event_type =='stake' || item.event_type=='initial_stake'" >
+								<div class="title is-6"><b>{{item.event_type =='stake' ? 'Counter stake' : 'Initial stake'}} </b> - {{item.time}}</div>
+
+								<div class="d-block text-break">
+									<b><user :address="item.author_address" :nickname="item.author_nickname"/></b>
+									staked <b><byte-amount :amount="item.accepted_amount"/></b> on <b>{{item.stake_on}}</b>
+								</div>
+								<span class="d-block">Resulting outcome: <b>{{item.new_outcome}}</b></span>
+								<span v-if="item.expected_reward" class="d-block">Expected reward: <b><byte-amount :amount="item.expected_reward"/></b></span>
+								<div class="progress-stacked mt-1">
+									<div class="bar" :style="{ height: 15 + 'px', background: '#48c774', width: ( item.total_staked_on_yes * 100) / (item.total_staked_on_yes + item.total_staked_on_no) + '%' }">
+										<byte-amount :amount="item.total_staked_on_yes"/>
+									</div>
+									<div class="bar" :style="{ height: 15 + 'px', background: '#f00', width: ( item.total_staked_on_no * 100) / (item.total_staked_on_yes + item.total_staked_on_no) + '%' }">
+										<byte-amount :amount="item.total_staked_on_no"/>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<div v-if="item.event_type =='withdraw'" >
-						<div cols="12">
-							<span><b>Withdraw</b> - {{item.time}} </span>
-							<div class="pt-2">
-								<span v-if="item.paid_out_amount" class="d-block"><b><byte-amount :amount="item.paid_out_amount"/></b> paid to <b>{{item.paid_out_address}}</b></span>
+							<div class="box" v-if="item.event_type =='commit'" >
+								<div class="title is-6"><b>Committed</b> - {{item.time}} </div>
+								<div class="d-block text-break">
+									<span v-if="item.paid_out_amount" class="d-block"><b><byte-amount :amount="item.paid_out_amount"/></b> paid to <b>{{item.paid_out_address}}</b></span>
+								</div>
+							</div>
+							<div class="box" v-if="item.event_type =='withdraw'" >
+								<div class="title is-6"><b>Withdraw</b> - {{item.time}} </div>
+								<div class="d-block text-break">
+									<span v-if="item.paid_out_amount" class="d-block"><b><byte-amount :amount="item.paid_out_amount"/></b> paid to <b>{{item.paid_out_address}}</b></span>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<hr>
 			</div>
-		</div>
 	</b-collapse>	
 </template>
 
@@ -119,14 +114,33 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-	.scrollable{
-	max-height: 10rem;
+.scrollable{
+	max-height: 12rem;
 	overflow: auto;
-	}
+}
 
-	.history-tile{
-		background-color: colors("light");
-		margin-top: 1rem;
-		margin-bottom: 1rem;
+.history-tile{
+	background-color: colors("light");
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+}
+
+.event-block{
+background-color: gainsboro;
+}
+.progress-stacked {
+display: flex;
+border-radius: 4px;
+overflow: hidden;
+.bar {
+	position: relative;
+	& > span {
+		display: block;
+		text-align: center;
+		line-height: 15px;
+		font-weight: bold;
+		color: #fff;
+	}
+}
 	}
 </style>
